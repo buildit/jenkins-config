cd5a9e3892f26aa80e3a2bbe23bdfb9d9fb8e50c
Dermot Burke <dermotmburke@gmail.com>
2018-03-23 15:35:34 +0000

    Add job dsl

---
+++ jobdsl/jobdsl.groovy
@@ -0,0 +1,32 @@
+pipelineJob('hello-k8s') {
+    definition {
+        cps {
+            sandbox()
+            script("""
+pipeline {
+    agent {
+        kubernetes {
+            label 'k8s'
+            inheritFrom 'base'
+            containerTemplate {
+                name 'alpine'
+                image 'alpine:3.7'
+                ttyEnabled true
+                command 'cat'
+            }
+        }
+    }
+    stages {
+        stage('say hello') {
+            steps {
+                script {
+                    sh("echo k8s")
+                }
+            }
+        }
+    }
+}
+      """.stripIndent())
+        }
+    }
+}
